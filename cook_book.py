c7482e9efa29f11cf5ba7edd8ce21d25931b20f9
SergeyDolgushin <sergei.dolgushin@mail.ru>
2021-12-14 21:06:48 +0300

    add all functions

--- cook_book.py
+++ cook_book.py
@@ -1,25 +1,50 @@
 import os
 
 def read_file(name_file):
-    all_recipes = {}
+    cook_book = {}
     ingredient = []
     file_path = os.getcwd()
     path_to_file = os.path.join(file_path, name_file)
-    # print(f'путь {path_to_file}')
     with open(path_to_file, 'rt', encoding='utf-8') as recipes:
         for line in recipes:
             meals = line.strip()
-            # print("Название", meals)
             count = int(recipes.readline().strip())
-            # print(count)
             for line in range(count):
                 record = recipes.readline().strip().split(' | ')
                 ingredient.append({'ingredient_name': record[0], 'quantity': record[1], 'measure': record[2]})
-                all_recipes[meals] = ingredient    
-                # print("Ингредиент:", ingredient)
+                cook_book[meals] = ingredient    
             recipes.readline().strip()
             ingredient = []    
-    return all_recipes  
+    return cook_book  
 
-print()
-print(read_file('recipes.txt'))
+
+def get_list_by_dishes(dishes, cook_book):
+    shop_list = []
+    for dish in dishes:
+        if dish not in cook_book:
+            print(f'Рецепта {dish} нет в кулинарной книге')
+            return -1    
+        for i, item in enumerate(cook_book[dish]):
+            if len(shop_list) == 0:
+                shop_list.append(item)
+            elif item not in shop_list:
+                shop_list.append(item)    
+            else:
+                shop_list[shop_list.index(item)]['quantity'] = str(int(shop_list[shop_list.index(item)]['quantity'])+int(item['quantity']))               
+    return shop_list 
+    
+    
+
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
+    shop_list_dishes = {}
+    shop_list = get_list_by_dishes(dishes, cook_book)
+    for ingredient in shop_list:    
+        volume_for_persons = int(ingredient['quantity']) * person_count
+        shop_list_dishes[ingredient['ingredient_name']] = {'measure': ingredient['measure'], 'quantity': str(volume_for_persons)}
+    return shop_list_dishes
+
+
+
+shop_list = get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2, read_file("recipes.txt"))
+# print(*shop_list.items(), sep = "\n")
+print(shop_list)
